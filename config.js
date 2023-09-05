ffb994f030248c292f7bbb57987581f7d0385425
Tr1tonZz <144108834+Tr1tonZz@users.noreply.github.com>
2023-09-05 16:02:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,16 @@
 const CONFIG = {
-    titleWeb: "đitmemay",
-    introTitle: 'Phanh an cut',
-    introDesc: ,
-    btnIntro: 'ấn vào đây hoặc m ăn c',
-    title: 'dm would u become my gf?',
-    desc: 'chọn yes hoặc m an c ',
-    btnYes: 'yes',
-    btnNo: 'Không (Phanh an c)',
-    question: 'ra là m thích t à :Đ',
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Tên người yêu bạn',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
